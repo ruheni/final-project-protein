110286cc9b1807c7e1a2a174b8981e909eaa47c8
Ariel Cobena <cobenaa11@gmail.com>
2023-07-11 11:29:39 -0700

    Add or

--- components/home/card.tsx
+++ components/home/card.tsx
@@ -46,7 +46,7 @@ export default function Card({
                 ),
               }}
             >
-              {description}
+              {description ?? ""}
             </ReactMarkdown>
           </Balancer>
         </div>
