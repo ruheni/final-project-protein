bdc4faf5b41b8d93093f3e61018cf76f125c0a1f
Ariel Cobena <cobenaa11@gmail.com>
2023-07-11 14:33:09 -0700

    Remove unused

--- components/home/default-home-cards.tsx
+++ components/home/default-home-cards.tsx
@@ -3,11 +3,10 @@ import Card from "@/components/home/card";
 export default async function DefaultHomeCards() {
     return (
         <div className="my-10 grid w-full max-w-screen-xl animate-fade-up grid-cols-1 gap-5 px-5 md:grid-cols-3 xl:px-0">
-        {features.map(({ title, description, demo, large }) => (
+        {features.map(({ title, demo, large }) => (
             <Card
               key={title}
               title={title}
-              description={description}
               demo={demo}
               large={large}
             />
